15076ef10843e3a360110aa688f8f155f45a9537
OwaisRaza <owaisraza1989@gmail.com>
2021-10-07 14:12:13 +0500

    first rxjs code example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { from } from "rxjs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const promise = [
+    {
+      name: "jon",
+    },
+    {
+      name: "Doe",
+    },
+    {
+      name: "kz",
+    },
+    {
+      name: "ovais",
+    },
+  ];
+  const promiseObservable$ = from(promise);
+
+  promiseObservable$._subscribe({
+    next: (data) => console.log(data),
+    error: (error) => console.log(error),
+    complete: () => console.log("Done "),
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
